d957973056695cb7d6c64fd9f73d2c3d3c12d1ff
Chris Fanning <chris@cfan.dev>
2023-07-22 10:57:53 -0500

    Add zypper for opensuse, add sdk man, add chrome

--- bin/install.dotfile.sh
+++ bin/install.dotfile.sh
@@ -8,6 +8,21 @@ mv ~/.zsh_plugins.txt ~/.zsh_plugins.txt.before.cfan.dotfiles
 ln -s ~/dotfiles/src/zshrc ~/.zshrc
 ln -s ~/dotfiles/src/zsh_plugins.txt ~/.zsh_plugins.txt 
 
-sudo dnf install gcc g++ ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop bpytop emacs
+# sudo dnf install gcc g++ ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop bpytop emacs
+
+sudo zypper ar http://dl.google.com/linux/chrome/rpm/stable/x86_64 Google-Chrome
+
+wget https://dl.google.com/linux/linux_signing_key.pub
+sudo rpm --import linux_signing_key.pub
+sudo zypper ref -f
+
+sudo zypper install gcc gpp ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop btop emacs git dropbox kubectx zsh kio-gdrive google-chrome-stable zip
+
+
+
+
+# Install SDKMan
+curl -s "https://get.sdkman.io" | bash
+
 
 
