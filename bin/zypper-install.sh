1da4e5f661c4923d5971e852a22599de7c745850
Chris Fanning <chris@cfan.dev>
2023-08-06 15:04:15 -0500

    Add zypper install script

---
+++ bin/zypper-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+sudo zypper ref -f
+
+
+
+sudo zypper install gcc gpp ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop btop emacs git dropbox kubectx zsh kio-gdrive google-chrome-stable zip nodejs docker xbanish kubernetes-client docker docker-compose docker-compose-switch kubens k9s
+
+
+
