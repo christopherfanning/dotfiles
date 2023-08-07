eee457a156736d59d1283e0d6e329ce0fc806b96
Chris <chris@cfan.dev>
2023-08-06 22:33:21 -0500

    Add devel_basis

--- bin/zypper-install.sh
+++ bin/zypper-install.sh
@@ -8,4 +8,6 @@ sudo zypper ref -f
 sudo zypper install gcc gpp ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop btop emacs git dropbox kubectx zsh kio-gdrive google-chrome-stable zip nodejs docker xbanish kubernetes-client docker docker-compose docker-compose-switch kubens k9s
 
 
+sudo zypper install -t pattern devel_basis
+
 
