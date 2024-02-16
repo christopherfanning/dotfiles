127320f28890deb83a944b8d6e3206760de3aee9
Chris Fanning <chris@cfan.dev>
2024-02-16 16:28:27 -0600

    Add some new config

--- bin/install.dotfile.sh
+++ bin/install.dotfile.sh
@@ -10,11 +10,15 @@ mv ~/.zsh_plugins.txt ~/.zsh_plugins.txt.before.cfan.dotfiles
 ln -s ~/dotfiles/src/zshrc ~/.zshrc
 ln -s ~/dotfiles/src/zsh_plugins.txt ~/.zsh_plugins.txt
 ln -s ~/dotfiles/src/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/fonts ~/.fonts
 
 # TODO 
 # ln -s ~/dotfiles/src/tmux ~/.config/tmux
+# ln -s ~dotfiles/src/nvim ~/.config/nvim
 
-
+# Create directory for Jetbrains commandline tools.
+mkdir ~/.local/share/JetBrains/Toolbox/apps
+mkdir ~/.local/share/JetBrains/Toolbox/scripts
 
 
 # sudo dnf install gcc g++ ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop bpytop emacs
@@ -24,9 +28,32 @@ ln -s ~/dotfiles/src/tmux/tmux.conf ~/.tmux.conf
 # wget https://dl.google.com/linux/linux_signing_key.pub
 # sudo rpm --import linux_signing_key.pub
 
-sudo zypper ref -f
-sudo zypper install gcc gpp ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop btop emacs git dropbox kubectx zsh kio-gdrive google-chrome-stable zip nodejs docker xbanish kubernetes-client docker docker-compose docker-compose-switch kubens k9s
+# sudo apt install gcc gpp ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop btop emacs git dropbox kubectx zsh kio-gdrive google-chrome-stable zip nodejs docker xbanish kubernetes-client docker 
 
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Docker Install
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo apt install gcc gpp ranger wget curl neovim neofetch openssl netcat tmux fzf bat kitty tmux htop emacs git zsh kio-gdrive zip nodejs docker xbanish docker docker-compose tree
+
+sudo apt-get install build-essential most wireshark-qt ripgrep fd-find spellcheck
+
+
+# add to bashrc
+# export PAGER=“most”
 
 
 # Install Antidote
@@ -35,11 +62,34 @@ git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antid
 # Install TPM for tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+
 # Install SDKMan
 curl -s "https://get.sdkman.io" | bash
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+brew install nvim
 
+# Install Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
+# Install NVM
+echo "install NVM"
+echo "https://github.com/nvm-sh/nvm?tab=readme-ov-file#install--update-script"
+
+echo "Then run:"
+echo "  nvm install --lts"
+
+
+# Install fix for gdrive ubuntu
+
+# Reload fonts
+fc-cache -f -v
+
+
+# test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+# test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.bashrc
 
