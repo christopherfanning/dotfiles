447d4c284147a1099d58d062fd2570c784bf9449
cfan <chris@cfan.dev>
2025-02-08 14:34:11 -0600

    idk, saving before reinstall

---
+++ src/.wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'MesloLGS Nerd Font Mono'
+config.color_scheme = 'Catppuccin Mocha'
+
+return config
+
+
--- src/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require('plugins')
-
-vim.opt.relativenumber = true
-vim.opt.number = true
-
-
