62c7039771ba55d6b843f803417c4d81ce72c625
Chris Fanning <chris@cfan.dev>
2025-02-09 14:00:15 -0600

    Add arch scripts

---
+++ bin/arch-install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+# basic programs
+#
+
+# timeshift or snapper or something:
+# take snapshot
+#
+
+
+# Update system?
+#
+#
+
+# Mainline packages
+sudo pacman -S blender gimp inkscape godot-mono neovim wezterm htop git obsidian audacity yazi fd ripgrep gnome-disk-utility base-devel 
+
+
+
+# AUR Packages 
+# Aseprite
+# jetbrains-toolbox
+# insomnia
+#
+
